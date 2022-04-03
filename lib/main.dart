1fc91dd2e4b671033c607cba492871f8041b95cc
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-04-03 12:33:50 +0000

    Finido
    Klárt

--- lib/main.dart
+++ lib/main.dart
@@ -3,84 +3,83 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
+//This is  the app
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.red,
         body: SafeArea(
+          //This is the picture
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('images/kristjan.jpg')),
+            //Name
             Text(
               'Kristján Sigurðsson',
               style: TextStyle(
                   fontFamily: 'PermanentMarker',
                   fontSize: 40.0,
-                  color: Colors.white,
+                  color: Colors.black,
                   fontWeight: FontWeight.bold),
             ),
+            //Occupation
             Text(
-              'Flutter Student',
+              'FLUTTER STUDENT',
               style: TextStyle(
                 fontFamily: 'SourceSansPro',
                 fontSize: 30.0,
-                color: Colors.grey.shade400,
+                color: Colors.black,
                 letterSpacing: 2.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.phone,
-                    color: Colors.black,
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    '+354 698 0654',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20.0,
-                    ),
-                  )
-                ],
+            //This is the line inbetween
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.black,
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white,
+            //Card instead of a row for the phone and email
+            Card(
               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.email,
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.black,
+                ),
+                title: Text(
+                  '+354 698 0654',
+                  style: TextStyle(
                     color: Colors.black,
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20.0,
                   ),
-                  SizedBox(
-                    width: 10.0,
+                ),
+              )
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.black,
+                ),
+                title: Text(
+                  'kristjansig1983@gmail.com',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20.0,
                   ),
-                  Text(
-                    'kristjansig1983@gmail.com',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20.0,
-                    ),
-                  )
-                ],
-              ),
+                ),
+              )
             )
           ],
         )),
@@ -88,3 +87,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
